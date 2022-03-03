49cb63071026a9eee5fb9518eb3a91d4515d279f
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-03 09:39:07 +0500

    add prototype

--- 14-OOP-JS/src/index.js
+++ 14-OOP-JS/src/index.js
@@ -4,7 +4,7 @@
 const Person = function(firstName, birthYear) {
   
   // 1
-  console.log(this);
+  // console.log(this);
   //* output is: Person {}
 
   //! 2 INSTANSE PROPERTIES
@@ -13,9 +13,9 @@ const Person = function(firstName, birthYear) {
   this.birthYear = birthYear;
 
   //? METHODS, don't use this
-  this.calcAge = function() {
-    console.log(2037 - this.birthYear);
-  };
+  // this.calcAge = function() {
+  //   console.log(2037 - this.birthYear);
+  // };
 
 } 
 // 1
@@ -31,17 +31,42 @@ const jonas = new Person('Jonas', 1991)
 console.log('jonas: ', jonas);
 
 // 3 
-const susan = new Person('Susan', 2017)
-const jack = new Person('Jack', 1975)
-console.log(susan, jack);
+// const susan = new Person('Susan', 2017)
+// const jack = new Person('Jack', 1975)
+// console.log(susan, jack);
 
 //! Object, created from a class is called an instance
 //? jonas, susan, jack are instance of Person
 // check
-console.log(jonas instanceof Person);  //* true
+// console.log(jonas instanceof Person);  //* true
 
 // but
 const jay = 'Jay'
-console.log(jay instanceof Person);  //* false
+// console.log(jay instanceof Person);  //* false
+
+
+//todo PROTOTYPES
+
+console.log(Person.prototype);
+//add method to Person
+Person.prototype.calcAge = function() {
+  console.log( 2037 - this.birthYear)
+}
+
+jonas.calcAge()  //* 46
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);  //* true
+console.log(Person.prototype.isPrototypeOf(jonas)); //* true 
+//! but, KEEP IN MIMD 
+console.log(Person.prototype.isPrototypeOf(Person)); //* false 
+
+//! .prototypeOfLinkedObjects
+// try to understand it
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species);  //* Homo Sapiens
+// and
+console.log(jonas.hasOwnProperty('firstName'));  //* true, comes from PERSON
+console.log(jonas.hasOwnProperty('species'));  //* false, , comes from PROTOTYPE, see consol 
 
 
