1d26553a36a123fcc56edae06fb0e0ebadca2dba
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-05 14:43:18 +0500

    update render

--- 15-API/src/script.js
+++ 15-API/src/script.js
@@ -31,7 +31,7 @@ class Workout {            //    6
     // prettier-ignore
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-    this.description = `${this.type[0].toUppercase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`
   }
 }
 
@@ -295,8 +295,10 @@ class App {
               <span class="workout__unit">m</span>
             </div>
           </li>  
-      `;
+      `;      
     }
+
+    form.insertAdjacentHTML('afterend', html);
   }
 
 }
