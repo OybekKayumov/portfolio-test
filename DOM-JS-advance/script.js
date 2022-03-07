354b4c5c25ae20b613af0d0fe10402763e883bd0
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-07 18:48:38 +0500

    add select nav element mouseout

--- DOM-JS-advance/script.js
+++ DOM-JS-advance/script.js
@@ -406,5 +406,17 @@ nav.addEventListener('mouseover', function(e) {
 
 // opposite of mouseover is:
 nav.addEventListener('mouseout', function(e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach((el) => {
+      if (el !== link) {
+        el.style.opacity = 1;
+      }        
+    });
+    logo.style.opacity = 1;
+  }    
   
 })
