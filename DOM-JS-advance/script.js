3139e08054fbecde20ccab07ea3c71101f66c106
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-07 18:46:00 +0500

    add select nav element mouseover

--- DOM-JS-advance/script.js
+++ DOM-JS-advance/script.js
@@ -392,8 +392,15 @@ const nav = document.querySelector('.nav');
 nav.addEventListener('mouseover', function(e) {
     if (e.target.classList.contains('nav__link')) {
       const link = e.target;
-      const siblings = link.closest('.nav').querySelector('.nav__link');
+      const siblings = link.closest('.nav').querySelectorAll('.nav__link');
       const logo = link.closest('.nav').querySelector('img');
+
+      siblings.forEach((el) => {
+        if (el !== link) {
+          el.style.opacity = 0.5;
+        }        
+      });
+      logo.style.opacity = 0.5;
     }    
 })
 
