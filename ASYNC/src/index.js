c0d4ea94440915f0ff5e31bbed03b6c5738232a2
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-09 11:50:59 +0500

    add V.2 multiply countries, V.3 not fully works

--- ASYNC/src/index.js
+++ ASYNC/src/index.js
@@ -129,39 +129,40 @@ const getCountryData = function(country) {
 //*  Португалия
 
 //TODO 2: Reuse code to create an element for multiple countries
-getCountryData('portugal')
+// getCountryData('portugal')
 // getCountryData('uzbekistan')
 
 
 // create function for v.2
 
-// const getCountryDataV2 = function (country) {
-//   const request = new XMLHttpRequest();
-//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-//   request.send();
+const getCountryDataV2 = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
   
-//   request.addEventListener('load', function() {
-//     const [data] = JSON.parse(this.responseText);
-//     console.log('data: ', data);
+  request.addEventListener('load', function() {
+    const [data] = JSON.parse(this.responseText);
+    console.log('data: ', data);
 
-//     const html = `
-//       <article class="country">
-//         <img class="country__img" src="${data.flag}" />
-//         <div class="country__data">
-//           <h3 class="country__name">${data.name}</h3>
-//           <h4 class="country__region">${data.region}</h4>
-//           <p class="country__row"><span>👫</span>${(
-//             +data.population / 1000000
-//           ).toFixed(1)} people</p>
-//           <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//           <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-//         </div>
-//       </article>
-//     `;
+    const html = `
+      <article class="country">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} people</p>
+          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+          <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+      </article>
+    `;
 
-//     countriesContainer.insertAdjacentHTML('beforeend', html);
-//     countriesContainer.style.opacity = 1;    
-//   })
-// }
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;    
+  })
+}
 
-// getCountryDataV2('portugal')
+getCountryDataV2('portugal')
+getCountryDataV2('uzbekistan')
