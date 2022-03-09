109d13277e29b4c478649745608bbf57178c1526
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-09 09:46:58 +0500

    add async

---
+++ ASYNC/src/index.js
@@ -0,0 +1,34 @@
+// 1
+// const p = document.querySelector('p');
+
+// setTimeout(() => {
+//   p.textContent = 'My name is JavaScript!'
+// }, 3000);
+
+// p .style.color = 'red'
+
+// setTimeout(() => {console.log("this is the first message")}, 5000);
+// setTimeout(() => {console.log("this is the second message")}, 3000);
+  
+// setTimeout(() => {
+//   console.log("this is the third message");
+//   p.innerHTML = 'My name is JavaScript!';
+// }, 1000);
+
+// [1, 2, 3].map(v => v * 2);
+//callback functions alone DO NOT make code asynchronous
+
+// 2
+// const img = document.querySelector('.bear');
+// img.src = 'img/bear.jpg';
+// img.addEventListener('load', function() {
+//   img.classList.add('fadeIn');
+// });
+
+// p.style.width = '300px';
+// p.style.fontSize = '24px';
+// p.style.fontFamily = 'Rubik';
+
+//! JSON is basically JavaScript Object, but converted to a String
+
+// alert('1')
