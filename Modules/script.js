e45c4dd31854c31b985bd3275641df2751c79f1f
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-11 18:56:59 +0500

    add modules: 2 types of export

---
+++ Modules/script.js
@@ -0,0 +1,6 @@
+// Importing module
+import './shoppingCart.js'
+
+console.log('Importing module');
+console.log(shippingCost);
+//! Uncaught ReferenceError: shippingCost is not defined
---
+++ Modules/shoppingCart.js
@@ -0,0 +1,11 @@
+// Exporting module
+// this code will executed FIRST
+
+console.log('Exporting module');
+
+const shippingCost = 10;
+const cart = [];
+
+//? 2 types of export
+// Named and Default Exports
+
