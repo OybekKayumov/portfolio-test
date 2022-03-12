1d7aa64e67733cb98c53ed59ee7e5fcdd50c2945
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-12 08:21:41 +0500

    update modules: commonJS module - require

--- Modules/script.js
+++ Modules/script.js
@@ -84,30 +84,42 @@ import * as ShoppingCart from './shoppingCart.js'
 
 
 
-const ShoppingCart2 = (function() {
-  const cart = [];
-  const shippingCost = 10;
-  const totalPrice = 237;
-  const totalQuantity = 23;
+// const ShoppingCart2 = (function() {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
 
-  const addToCart = (product, quantity) => {
-    cart.push({ product, quantity });
-    console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
-  };
+//   const addToCart = (product, quantity) => {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
+//   };
 
-  const orderStock = (product, quantity) => {
-    console.log(`${quantity} ${product} ordered from supplier`)
-  } 
+//   const orderStock = (product, quantity) => {
+//     console.log(`${quantity} ${product} ordered from supplier`)
+//   } 
   
-  return {
-    addToCart,
-    cart,
-    totalPrice,
-    totalQuantity,
-  }
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   }
 
-})()
+// })()
 
-ShoppingCart2.addToCart('apple', 4)
-ShoppingCart2.addToCart('pizza', 2)
-console.log('Array cart: ', ShoppingCart2.cart);
+// ShoppingCart2.addToCart('apple', 4)
+// ShoppingCart2.addToCart('pizza', 2)
+// console.log('Array cart: ', ShoppingCart2.cart);
+
+
+//! CommonJS module
+// export
+export const addToCart3 = (product, quantity) => {
+      cart.push({ product, quantity });
+      console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
+    };
+
+// import
+const { addToCart3 } = require ('./shoppingCart.js')
+  
