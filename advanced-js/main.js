4a1bbfc45eba7b115382abaf5b158aa21837f8af
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-14 06:44:25 +0500

    add closures

--- advanced-js/main.js
+++ advanced-js/main.js
@@ -15,6 +15,7 @@ outer()
 // Javascript начинает искать с внутреннеой функции и идет на уровень верх если не найдет нужную переменную.
 // вложенные функции имеют доступ к переменны, оъявленным в своей собственной области. а также к переменным, объявленным во внешней области.
 
+// 2
 function outer1() {
   let counter = 0
   function inner1() {
@@ -24,10 +25,28 @@ function outer1() {
   inner1()
 }
 
-outer1();
-outer1();
+// outer1();
+// outer1();
 //* counter:  1
 //* counter:  1
 
-// closures завыкания
+
+// closures замыкания
+// 3
+function outer2() {
+  let counter = 0
+  function inner2() {
+    counter++;
+    console.log('counter: ', counter);
+  }
+  return inner2;
+}
+
+const fn = outer2();
+fn();
+fn();
+// counter:  1
+// counter:  2
+// когда возращаем функцию из другой функции мы фактически возвращаем комбинацию function definition вместе с областью действия function's scope.
+// это позволило бы function definition иметь связанную посьоянную память, которая могла бы удерживать live data между выполнениями. эта комбинация функции и и ее цепочки области действия - называется замыканием - Closure
  
