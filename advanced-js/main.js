185be62f3d2a2782c8199d8b43bf51f56a455201
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-14 08:38:51 +0500

    add this 4 types

--- advanced-js/main.js
+++ advanced-js/main.js
@@ -12,8 +12,8 @@ function outer() {
 
 outer() 
 //* a. b, c : 10 20 30
-// Javascript начинает искать с внутреннеой функции и идет на уровень верх если не найдет нужную переменную.
-// вложенные функции имеют доступ к переменны, оъявленным в своей собственной области. а также к переменным, объявленным во внешней области.
+// Javascript начинает искать с внутренней функции и идет на уровень верх если не найдет нужную переменную.
+// вложенные функции имеют доступ к переменным, оъявленным в своей собственной области, а также к переменным, объявленным во внешней области.
 
 // 2
 function outer1() {
@@ -48,7 +48,7 @@ fn();
 // counter:  1
 // counter:  2
 // когда возращаем функцию из другой функции мы фактически возвращаем комбинацию function definition вместе с областью действия function's scope.
-// это позволило бы function definition иметь связанную посьоянную память, которая могла бы удерживать live data между выполнениями. эта комбинация функции и и ее цепочки области действия - называется замыканием - Closure
+// это позволило бы function definition иметь связанную постоянную память, которая могла бы удерживать live data между выполнениями. эта комбинация функции и ее цепочки области действия называется замыканием - Closure
  
 
 //! Function currying
@@ -80,3 +80,78 @@ const add3 = add2(3)
 const add5 = add3(5)
 
 console.log('add5: ', add5);
+
+
+//! this
+// используется в функции. И относится к объекту, которому оно принадлежит и делает функцию многоразовым, позволяя выбрать значение объекта. Значение THIS полностью определяется тем, как сейчас ф-я вызывается.
+// как использовать THIS и как его значение изменяется на основе вызовы функции
+
+    // function sayMyName(name) {
+    //   console.log(`Hello, my name is ${name}!`);
+    // }
+
+    // sayMyName('Walter White')
+
+//! How to determine 'THIS' ? определить
+//implicit binding неявная привязка
+//exlicit binding явная привязка
+//new binding новая привязка
+//default binding привязка по умолчанию
+
+//todo implicit binding неявная привязка покажет к чему относится 
+const person = {
+  name: 'Vishwas',
+  sayMyName: function () {
+      console.log(`Hello, my name is ${this.name}!`);
+    },
+}
+
+// person.sayMyName()
+
+// when a function is invoked with the dot notation, 
+// the object to the left of the dot is what THIS is referencing
+// JavaScript will now treat THIS.name as person.name
+// which is equal to the string 'Vishwas' 
+
+
+//todo explicit binding 
+
+function sayMyName () {
+  console.log(`Hey, my name is ${this.name}!`);
+}
+
+sayMyName.call(person)
+// Hey, my name is Vishwas!
+
+
+//todo new binding 
+// const name = 'Hulk'
+// globalThis.name = 'Hulk'
+
+function Person (name) {
+  // this = {}
+  this.name = name  
+}
+
+const p1 = new Person('Batman')
+const p2 = new Person('Superman')
+
+console.log(p1, p2);
+console.log(p1.name, p2.name);
+
+// Person {name: 'Batman'} Person {name: 'Superman'}
+// Batman Superman
+
+
+//todo default binding 
+sayMyName()
+// Hey, my name is !
+
+globalThis.name = 'Hulk'
+// Hey, my name is Hulk!
+
+//! Order of precedence
+// New binding
+// explicit binding
+// imlicit binding
+// default binding
