5999b8287353a25f6d74417011a43aa2ebfca152
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-18 15:13:38 +0500

    add this: object, object method

--- advanced-js/script.js
+++ advanced-js/script.js
@@ -6,4 +6,54 @@
 // Method, 
 // Class
 
-//? this inside Global Scope
+//! this inside Global Scope
+// house
+this.table = 'window table!';
+console.log(window.table);
+
+this.garage = {
+  table: 'garage table',
+  cleanTable() {
+      console.log(`cleaning ${this.table}`);
+  }
+};
+
+console.log(this.garage.table);
+// or
+console.log(window.garage.table);
+
+// window is GLOBAL SCOPE
+
+
+//! this inside OBJECT
+let JohnsRoom = {
+  table: 'Johns table'
+}
+
+// we cannot use this in private variables
+// console.log(this.JohnsRoom.table);
+//! Uncaught TypeError: Cannot read properties of undefined (reading 'table')
+
+// we can use like this:
+// console.log(JohnsRoom.table);
+
+
+//! this inside METHOD
+// create method inside JohnsRoom
+
+let JohnsRoomMethod = {
+    table: 'Johns table', 
+    cleanTable() {    //? cleanTable() is METHOD
+      console.log(`cleaning ${this.table}`);
+    }
+}
+
+console.log(JohnsRoomMethod.table);
+//* Johns table
+
+// console.log(JohnsRoomMethod.cleanTable());
+
+JohnsRoomMethod.cleanTable()
+//* cleaning Johns table
+
+this.garage.cleanTable()
