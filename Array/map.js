486b50e144877f4619895cf1f3e8addec44a9c5f
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-21 08:47:53 +0500

    add map object

--- Array/map.js
+++ Array/map.js
@@ -19,3 +19,15 @@ console.log('newArr: ', newArr);
 //* Array:  (4) [3, 4, 5, 6]
 //* newArr:  (4) [9, 12, 15, 18]
 
+//todo use map() over an array of objects
+let books = [
+  {author: 'Author 1', title: 'Title 1'},
+  {author: 'Author 2', title: 'Title 2'},
+  {author: 'Author 3', title: 'Title 3'},
+]
+
+let book = books.map((elem) => {
+  return `${elem.title} by ${elem.author}`
+})
+
+console.log('books by authors: ', book);
