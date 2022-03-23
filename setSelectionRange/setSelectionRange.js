ed6443f25d6c3767b412eebdc32804f8a1951913
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-23 11:23:09 +0500

    add setSelectionRange folder

---
+++ setSelectionRange/setSelectionRange.js
@@ -0,0 +1,15 @@
+// Метод HTMLInputElement.setSelectionRange() устанавливает начальное и конечное положение выделения текста в элементе <input>
+
+// В более новых версиях браузеров, можно дополнительно установить направление выделения текста, что позволит, например, определить, что выделение сделано нажатием и перетаскиванием курсора мыши от конца выделенного текста до начала.
+
+// Этот метод позволяет одним вызовом обновить свойства HTMLInputElement.selectionStart, selectionEnd, и selectionDirection
+
+// Syntax
+// inputElement.setSelectionRange(selectionStart, selectionEnd, [optional] selectionDirection);
+
+// selectionStart - индекс первого выделенного символа,
+// selectionEnd - индекс последнего выделенного символа,
+// selectionDirection - [optional] строка, определяющая направления произведенного выделения. Принимаются значения "forward", "backward", или "none", если направление выделения неизвестно или неважно.
+
+
+
