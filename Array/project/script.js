d92054c629b7ae3a740aecb540263cfcf2a71e04
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-24 17:15:42 +0500

    add html to js

--- Array/project/script.js
+++ Array/project/script.js
@@ -63,7 +63,16 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 
 const displayMovements = function(movements) {
+  movements.forEach((mov, i) => {
 
+    const type = mov > 0 ? 'deposit' : 'withdrawal'
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>    
+    `;
+  })
 }
 
 displayMovements(account1.movements)
