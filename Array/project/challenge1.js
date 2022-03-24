9d0ca822337738ee07219a512ea426e72c775949
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-24 19:32:41 +0500

    add test

--- Array/project/challenge1.js
+++ Array/project/challenge1.js
@@ -15,10 +15,12 @@ const checkDogs = (dogsJulia, dogsKate) => {
     if (dog >= 3) {
       console.log(`Dog number ${index + 1} is an adult and it is ${dog} years old`);
     } else {
-      console.log(`Dog number ${index + 1} is still a puppy (${dog} years old)`);
+      console.log(`Dog number ${index + 1} is still a puppy 🐶 (${dog} years old)`);
     }
   }) 
 }
 
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3])
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3])  //*(7) [5, 2, 4, 1, 15, 8, 3]
        //arr: dogsJulia,        dogsKate
+//test
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4])   //*(7) [16, 6, 10, 5, 6, 1, 4]
