dab4ccbb58464ab299e44bf056d877da88e4ade1
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-24 11:10:18 +0500

    add foreach

--- Array/project/script.js
+++ Array/project/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 let arr = ['a', 'b', 'c', 'd', 'e']
@@ -80,27 +80,27 @@ let arr = ['a', 'b', 'c', 'd', 'e']
 // we can extract part of any array WITHOUT changing the original array
 arr.slice(2) //* start from 'c' and all the way to the end
 // and doesn't mutate original arr. Returns new arr, copy of arr BUT only with the extracted parts
-console.log(arr.slice(2));  //* (3) ['c', 'd', 'e']
+// console.log(arr.slice(2));  //* (3) ['c', 'd', 'e']
 
-console.log(arr.slice(2, 4));  //* (2) ['c', 'd']
+// console.log(arr.slice(2, 4));  //* (2) ['c', 'd']
 // end parameter is not included in the output
 // (2, 4) means 2 and 3 without 4
 
-console.log(arr.slice(-2));  
+// console.log(arr.slice(-2));  
 // starts from the end, and takes last 2 elements of arr
 //*  (2) ['d', 'e']
 
-console.log(arr.slice(-1));  
+// console.log(arr.slice(-1));  
 // last element of any ARRAY
 
-console.log(arr.slice(1, -2)); //* (2) ['b', 'c']
+// console.log(arr.slice(1, -2)); //* (2) ['b', 'c']
 
 
 //! SHALLOW copy of arr
-console.log(arr.slice()); //* (5) ['a', 'b', 'c', 'd', 'e']
+// console.log(arr.slice()); //* (5) ['a', 'b', 'c', 'd', 'e']
 // same as:  
 //todo using spread operator
-console.log([...arr]); //* (5) ['a', 'b', 'c', 'd', 'e']
+// console.log([...arr]); //* (5) ['a', 'b', 'c', 'd', 'e']
 
 
 //todo splice
@@ -118,21 +118,62 @@ console.log([...arr]); //* (5) ['a', 'b', 'c', 'd', 'e']
 
 
 //todo reverse
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse()); //* 5) ['f', 'g', 'h', 'i', 'j']
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse()); //* 5) ['f', 'g', 'h', 'i', 'j']
 // reverse() mutates that array
-console.log(arr2); //* 5) ['f', 'g', 'h', 'i', 'j']
+// console.log(arr2); //* 5) ['f', 'g', 'h', 'i', 'j']
 
 //todo concat
 
-const letters = arr.concat(arr2);
-console.log(letters);
+// const letters = arr.concat(arr2);
+// console.log(letters);
 //* (10) ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
-console.log([...arr, ...arr2]);
+// console.log([...arr, ...arr2]);
 //* (10) ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
 
 //todo join
-console.log(letters.join(' - '));
+// console.log(letters.join(' - '));
 //* a - b - c - d - e - f - g - h - i - j
+
+
+//todo forEach
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    // console.log(`You withdrew ${movement}`);  //*  -
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+};
+
+console.log('----- FOREACH -----');
+movements.forEach((movement) => {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    // console.log(`You withdrew ${movement}`);  //*  -
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+})
+
+//* You deposited 200
+// You deposited 450
+// You withdrew 400
+// You deposited 3000
+// You withdrew 650
+// You withdrew 130
+// You deposited 70
+// You deposited 1300
+// ----- FOREACH -----
+// You deposited 200
+// You deposited 450
+// You withdrew 400
+// You deposited 3000
+// You withdrew 650
+// You withdrew 130
+// You deposited 70
+// You deposited 1300
