c5d53f5c13d3ec03e1bb1f5b52a8f67fc97e6c45
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-04 20:14:42 +0500

    API - ?

--- Repeat/async/index.js
+++ Repeat/async/index.js
@@ -6,3 +6,6 @@
 //? AJAX = asynchronous JavaScript And XML - allows to communicate with remote web servers in an asynchronous way. 
 //? with AJAX calls you can REQUEST DATA from web servers DYNAMICALLY
 
+// client request - response webserver
+// GET, POST, PUT...
+// WEB API - Application Programming Interface: PIECE OF SOFTWARE THAT CAN BE USED BY ANOTHER PIECE OF SOFTWARE, in order to allow applications to talk to each other and exchange information
