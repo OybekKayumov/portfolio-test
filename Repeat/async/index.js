1d3724f9db2fea2d23b644dc900e13bf4a1c02ed
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-04 21:23:25 +0500

    get gameID

--- Repeat/async/index.js
+++ Repeat/async/index.js
@@ -14,3 +14,44 @@
 // xml(no API uses anymore),
 // json(): JavaScript Object, converted to a String
 // text() ...
+
+class LocStorManager {
+  save = (obj) => {
+    const strObj = JSON.stringify(obj);
+    window.localStorage.setItem('object1', strObj);
+  }
+
+  retrieve = () => {
+    const serialObj = window.localStorage.getItem('object1');
+    const obj = JSON.parse(serialObj) || null;
+
+    return (obj === null) ? null : obj.gameID;
+  }
+}
+
+const storage = new LocStorManager();
+
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+
+const createGame = async () => {
+  const game = {
+    name: 'Tetris',
+  };
+
+  const response = await fetch(`${url}games`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(game),
+  });
+
+  const gameID = await response.json();
+  const str = gameID.result;
+
+  storage.save({ gameID: str });
+
+  return str;
+};
+
+console.log(createGame());
