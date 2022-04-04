a81405487a81320f7e1d70816797f3bf95e4a240
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-04 20:08:19 +0500

    async AJAX

---
+++ Repeat/async/index.js
@@ -0,0 +1,8 @@
+// addEventListener do not make code asynchronous
+// callback functions also not make code asynchronous
+// addEventListener listening for event and not doing any work in the background
+// and simply waiting for a click happen
+
+//? AJAX = asynchronous JavaScript And XML - allows to communicate with remote web servers in an asynchronous way. 
+//? with AJAX calls you can REQUEST DATA from web servers DYNAMICALLY
+
